9f4ef61bd0e51518544b4a759f1ca27f5e2225e3
Ted Whang <twmusic@Teds-MacBook-Pro-2.local>
2025-02-24 22:43:30 -0800

    Weather Proxy: Change methods to be class methods

--- app/proxies/weather_proxy.rb
+++ app/proxies/weather_proxy.rb
@@ -2,29 +2,31 @@ class WeatherProxy
   GEOCODING_BASE_API = "http://api.openweathermap.org/geo/1.0/zip"
   ONECALL_BASE_API = "https://api.openweathermap.org/data/3.0/onecall"
 
-  def initialize
-    @api_key = ENV['WEATHER_API_KEY']
-  end
+  class << self
+    def get_weather_data(zip_code:, country_code:)
+      lat, lon = get_lat_lon(zip_code: zip_code, country_code: country_code)
 
-  def get_weather_data(zip_code:, country_code:)
-    lat, lon = get_lat_lon(zip_code: zip_code, country_code: country_code)
+      url = "#{ONECALL_BASE_API}?lat=#{lat}&lon=#{lon}&exclude=minutely,hourly,alerts&appid=#{api_key}"
+      response = HTTParty.get(url)
+      JSON.parse(response.body)
+    end
 
-    url = "#{ONECALL_BASE_API}?lat=#{lat}&lon=#{lon}&exclude=minutely,hourly,alerts&appid=#{@api_key}"
-    response = HTTParty.get(url)
-    JSON.parse(response.body)
-  end
+    private
 
-  private
+    def get_lat_lon(zip_code:, country_code:)
+      url = "#{GEOCODING_BASE_API}?zip=#{zip_code},#{country_code}&appid=#{api_key}"
 
-  def get_lat_lon(zip_code:, country_code:)
-    url = "#{GEOCODING_BASE_API}?zip=#{zip_code},#{country_code}&appid=#{@api_key}"
+      response = HTTParty.get(url)
+      response_json = JSON.parse(response.body)
 
-    response = HTTParty.get(url)
-    response_json = JSON.parse(response.body)
+      lat = response_json["lat"]
+      lon = response_json["lon"]
 
-    lat = response_json["lat"]
-    lon = response_json["lon"]
+      [lat, lon]
+    end
 
-    [lat, lon]
+    def api_key
+      @api_key = ENV['WEATHER_API_KEY']
+    end
   end
 end
--- spec/proxies/weather_proxy_spec.rb
+++ spec/proxies/weather_proxy_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe WeatherProxy, type: :model do
   let(:zip_code) { '10001' }
   let(:country_code) { 'US' }
-  let(:weather_proxy) { WeatherProxy.new }
 
   describe '#get_weather_data' do
     it 'returns weather data for a given zip and country code' do
       result = 
         VCR.use_cassette('get_weather_data') do
-          weather_proxy.get_weather_data(zip_code: zip_code, country_code: country_code)
+          WeatherProxy.get_weather_data(zip_code: zip_code, country_code: country_code)
         end
 
       expect(result['current']['dt']).to be_present
@@ -28,7 +27,7 @@ RSpec.describe WeatherProxy, type: :model do
     it 'returns latitude and longitude for a given zip code and country code' do
       result =
         VCR.use_cassette('get_lat_lon') do
-          weather_proxy.send(:get_lat_lon, zip_code: zip_code, country_code: country_code)
+          WeatherProxy.send(:get_lat_lon, zip_code: zip_code, country_code: country_code)
         end
 
       expect(result).to eq([40.7484, -73.9967])
