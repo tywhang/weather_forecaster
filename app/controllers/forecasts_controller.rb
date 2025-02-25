91d04dfbceca0f62b4151d836a45d0afd3d6140c
Ted Whang <twmusic@Teds-MacBook-Pro-2.local>
2025-02-25 08:33:58 -0800

    Search: Handle blank zip code or country code

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -3,8 +3,18 @@ class ForecastsController < ApplicationController
   end
 
   def show
+    errors = []
+
     @zip_code = params[:zip_code]
     @country_code = params[:country_code]
+
+    errors << 'Address must have a zip code' if @zip_code.blank?
+    errors << 'Address must have a country code' if @country_code.blank?
+    if errors.any?
+      flash[:alert] = errors.join(', ')
+      redirect_to new_forecasts_path
+      return
+    end
 
     @weather_data = WeatherProxy.get_weather_data(zip_code: @zip_code, country_code: @country_code)
   rescue StandardError => e
--- spec/controllers/forecasts_controller_spec.rb
+++ spec/controllers/forecasts_controller_spec.rb
@@ -17,9 +17,42 @@ RSpec.describe ForecastsController, type: :controller do
       allow(WeatherProxy).to receive(:get_weather_data).with(zip_code: zip_code, country_code: country_code).and_return(weather_data)
     end
 
-    it 'returns a successful response' do
-      get :show, params: { zip_code: zip_code, country_code: country_code }
-      expect(response).to be_successful
+    context 'when zip code is blank' do
+      let(:zip_code) { '' }
+
+      it 'redirects to the new forecast page with an error message' do
+        get :show, params: { zip_code:, country_code: }
+        expect(response).to redirect_to(new_forecasts_path)
+        expect(flash[:alert]).to eq('Address must have a zip code')
+      end
+    end
+
+    context 'when country code is blank' do
+      let(:country_code) { '' }
+
+      it 'redirects to the new forecast page with an error message' do
+        get :show, params: { zip_code:, country_code: }
+        expect(response).to redirect_to(new_forecasts_path)
+        expect(flash[:alert]).to eq('Address must have a country code')
+      end
+    end
+
+    context 'when zip code and country code are blank' do
+      let(:zip_code) { '' }
+      let(:country_code) { '' }
+
+      it 'redirects to the new forecast page with an error message' do
+        get :show, params: { zip_code:, country_code: }
+        expect(response).to redirect_to(new_forecasts_path)
+        expect(flash[:alert]).to eq('Address must have a zip code, Address must have a country code')
+      end
+    end
+
+    context 'when zip code and country code are valid' do
+      it 'returns a successful response' do
+        get :show, params: { zip_code: zip_code, country_code: country_code }
+        expect(response).to be_successful
+      end
     end
   end
 end
