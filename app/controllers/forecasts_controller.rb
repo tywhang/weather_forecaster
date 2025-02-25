a5032a064da49fcbac0d55c815052fed6337571e
Ted Whang <twmusic@Teds-MacBook-Pro-2.local>
2025-02-25 00:25:52 -0800

    Search: Handle error cases from WeatherProxy

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -7,5 +7,8 @@ class ForecastsController < ApplicationController
     @country_code = params[:country_code]
 
     @weather_data = WeatherProxy.get_weather_data(zip_code: @zip_code, country_code: @country_code)
+  rescue StandardError => e
+    flash[:alert] = 'Unable to fetch weather data. Please try again later.'
+    redirect_to new_forecasts_path
   end
 end
